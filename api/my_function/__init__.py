cc7a99a64a3ae93f3a6d5600abd3608e002d28d6
Adrien.Palliez <adrienpalliez@gmail.com>
2025-02-14 13:06:27 +0100

    -

--- api/my_function/__init__.py
+++ api/my_function/__init__.py
@@ -29,6 +29,11 @@ def load_model_from_blob():
         blob_service_client = BlobServiceClient.from_connection_string(connect_str)
         container_client = blob_service_client.get_container_client(container_name)
 
+        # Download and load the ALS model (from cf_model.npz)
+        model_blob_client = container_client.get_blob_client("cf_model.npz")
+        with open("cf_model.npz", "wb") as download_file:
+            download_file.write(model_blob_client.download_blob().readall())
+
         # Load the ALS model's user and item factors
         model_data = np.load("cf_model.npz")
         user_factors = model_data['user_factors']
